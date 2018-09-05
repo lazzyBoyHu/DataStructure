4872ebb2d2c37e2f06a8908f0e8b499443b6abbd
hutao <787611491@qq.com>
2018-09-05 14:57:52 +0800

    插入排序

--- Sort/mysort.cpp
+++ Sort/mysort.cpp
@@ -9,5 +9,12 @@ InsertionSort::InsertionSort()
 
 void InsertionSort::sort()
 {
-    for(int i = 0; i < length; i++);
+    for(int i = 1; i < length; i++)
+    {
+        int tmp = std::move(arr[i]);
+        int j = 0;
+        for(j = i-1; j >= 0 && tmp < arr[i]; j++)
+            arr[j + 1] = std::move(arr[j]);
+        arr[j] = std::move(tmp);
+    }
 }
