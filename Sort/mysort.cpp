6464af8381eb2f7cd6eb5466a2daed520a70c78c
hutao <787611491@qq.com>
2018-09-05 15:14:30 +0800

    冒泡

--- Sort/mysort.cpp
+++ Sort/mysort.cpp
@@ -2,7 +2,27 @@
 
 #include "mysort.h"
 
+using std::move;
 
+//     -------------------   冒泡排序   ------------------
+BubbleSort::BubbleSort()
+{
+}
+void BubbleSort::sort()
+{
+    for(int i = 0; i < length; i++)
+        for(int j = 0; j < length - i; j++)
+            if(arr[j] > arr[j+1])
+            {
+                int tmp = move(arr[j+1]);
+                arr[j+1] = move(arr[j]);
+                arr[j] = move(tmp);
+            }
+}
+//     -------------------   冒泡排序   ------------------
+
+
+//     -------------------   插入排序   ------------------
 InsertionSort::InsertionSort()
 {
 }
@@ -11,10 +31,11 @@ void InsertionSort::sort()
 {
     for(int i = 1; i < length; i++)
     {
-        int tmp = std::move(arr[i]);
+        int tmp = move(arr[i]);
         int j = 0;
         for(j = i-1; j >= 0 && tmp < arr[i]; j++)
-            arr[j + 1] = std::move(arr[j]);
-        arr[j] = std::move(tmp);
+            arr[j + 1] = move(arr[j]);
+        arr[j] = move(tmp);
     }
 }
+//     -------------------   插入排序   ------------------
